2ed36b3d7acdb24113cba80c1cfe64cdd731a5e8
YangDongDa <2722978054@qq.com>
2023-04-12 14:52:47 +0800

    金属冶炼

--- everyday/P4956.cpp
+++ everyday/P4956.cpp
@@ -1,39 +1,31 @@
+#include <algorithm>
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-typedef struct Element
-{
-    long V;
-    long A;
-    long B;
-} Element;
-bool cmp(Element a, Element b)
-{
-    return a.V > b.V;
-}
-Element E[10001];
+// typedef struct Element
+// {
+//     long V;
+//     long A;
+//     long B;
+// } Element;
+// bool cmp(Element a, Element b)
+// {
+//     return a.V > b.V;
+// }
+// Element E[10001];
 int main (int argc, char *argv[])
 {
     int n;
-    long Max,Min;
+    long Max,Min,A,B;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> E[i].A >> E[i].B;
-        E[i].V = E[i].A / E[i].B;
+    cin >> A >> B;
+    Max = A / B;
+    Min = A / (B + 1) + 1;
+    for (int i = 2; i <= n; i++) {
+        cin >> A >> B;
+        Max = min(Max, A / B);
+        Min = max(Min, A / (B + 1) + 1);
         
-    }
-    sort(E + 1, E + n + 1, cmp);
-    Max = E[n].V;
-    Min = Max; 
-    // cout << Max << " ";
-    while(true){
-        if(Min * (E[1].B + 1) > E[1].A){
-            Min--;
-        }
-        else{
-            Min++;
-            break;
-        }
     }
     cout << Min<< " "<< Max << endl;
 
